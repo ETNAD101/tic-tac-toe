6556a8787bfd7c2c95e6c9772121439fcf701a4a
ETNAD101 <danterendell@gmail.com>
2022-06-08 19:48:36 -0400

    cleaned up

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//checks if chosen space is available
 bool checkPos(char arr[][3], int x, int y) {
   if(arr[y][x] == '-') {
     return true;
@@ -12,19 +13,21 @@ bool checkPos(char arr[][3], int x, int y) {
 }
 
 
-
+//places piece on the board by changing value in array
 int place(char arr[][3], int player, int pos) {
 	int x = 0;
 	int y = 0;
 	char piece;
-
+	
+	//sets piece depending on the player
 	if(player == 1) {
 		piece = 'X';
 	}
 	if(player == 2) {
 		piece = 'O';
 	}
- 
+	
+	//converts a number into a position on the game board array
 	for(int i = 0; i < pos; i ++) {
 		x++;
 		if(x > 2) {
@@ -32,8 +35,10 @@ int place(char arr[][3], int player, int pos) {
 			y++;
 		}
 	}
-  x--;
-
+	x--;
+	
+	//checks to see if the position in the array is available
+	//if available the piece will be placed
 	if(checkPos(arr, x, y)) {
 		arr[y][x] = piece;
 		return 0;
@@ -44,7 +49,7 @@ int place(char arr[][3], int player, int pos) {
 }
 
 
-
+//toggles the current player
 void togglePlayer(int *player) {
   if(*player == 1) {
     *player = 2;
@@ -54,7 +59,7 @@ void togglePlayer(int *player) {
 }
 
 
-
+//displays a 3x3 array in the console
 void dispArr(char arr[][3]) {	
 	cout << "-------";
 	cout << endl;
@@ -69,53 +74,73 @@ void dispArr(char arr[][3]) {
 }
 
 
-
+//main function
 int main()
 {
+	//initializing variables
 	int player = 1;
 	int pos;
+	int turns = 0;
 	bool run = true;
 	
+	//numbers on array corrospond to the game board positions
 	char tut[3][3] = {
 		{'1', '2', '3'}, 
 		{'4', '5', '6'},
 		{'7', '8', '9'}
 	};
     
+	//game board array
 	char game[3][3] = {
 		{'-', '-', '-'}, 
 		{'-', '-', '-'},
 		{'-', '-', '-'}
 	};
 	
+	//explain game
 	cout << "This is the game board.\n";
 	dispArr(tut);
 	cout << "When you are prompted to place a piece,\n";
 	cout << "select the position to place it in.\n";
 	
+	//main game loop
 	while(run) {
+		//counts the number of turns
+		turns ++;
+		
+		//player choses where to place piece
 		cout << "Player " << player << ", Select where to place a piece.\n";
 		dispArr(game);
-		
 		bool wait = true;
-    
-    while(wait){
-      cin >> pos;
-      if(pos > 9 || pos < 1) {
-        cout << "Invalid position\n";
-      }else {
-        wait = false;
-      }
-    }
-    
-	int checkPlace = place(game, player, pos);
-	if(checkWin(game, player) != 0) {
-		cout << "Player " << player <<" won!" << endl;
-		run = false;
-	}
-	if(checkPlace == 0)
-		togglePlayer(&player);
-	}
+		
+		while(wait){
+			cin >> pos;
+			//verifies piece will be placed on the board
+			if(pos > 9 || pos < 1) {
+				cout << "Invalid position\n";
+			}else {
+				wait = false;
+			}
+		}
+		
+		//places piece on the selected space if the space is free
+		int checkPlace = place(game, player, pos);
+
+		//checks to is if the last piece placed ended the game by either winning or a draw
+		if(checkWin(game, player) != 0) {
+			cout << "Player " << player <<" won!" << endl;
+			run = false;
+			dispArr(game);
+		}else if(turns == 9) {
+			cout << "Nobody won, Tie" << endl;
+			 run = false;
+		}
+		
+		//if the piece was succesfully placed change to the next player
+		//otherwise redo the current player's turn
+		if(checkPlace == 0)
+			togglePlayer(&player);
+		}
 	
     return 0;
 }
--- wincheck.cpp
+++ wincheck.cpp
@@ -3,26 +3,68 @@
 
 using namespace std;
 
-int wc1[3][2] = {
-	{0, 0},
-	{1, 0},
-	{2, 0}
+//win positions
+int wc1[8][3][2] = {
+	{
+		{0, 0},	//X--
+		{1, 0},	//X--
+		{2, 0}  //X--
+	},
+	{
+		{0, 1}, //-X-
+		{1, 1}, //-X-
+		{2, 1}  //-X-
+	},
+	{
+		{0, 2}, //--X
+		{1, 2}, //--X
+		{2, 2}  //--X
+	},
+	{
+		{0, 0}, //XXX
+		{0, 1}, //---
+		{0, 2}  //---
+	},
+	{
+		{1, 0}, //---
+		{1, 1}, //XXX
+		{1, 2}  //---
+	},
+	{
+		{2, 0}, //---
+		{2, 1}, //---
+		{2, 2}  //XXX
+	},
+	{
+		{0, 0}, //X--
+		{1, 1}, //-X-
+		{2, 2}  //--X
+	},
+	{
+		{0, 2}, //--X
+		{1, 1}, //-X-
+		{2, 2}  //X--
+	}
 };
 
 
-
-void coords(int row, int *x, int *y) {
-	*x = wc1[row][0];
-	*y = wc1[row][1];
+//grabs the coords from the win conditions array
+//and assigns the x and y values to variables
+void coords(int pattern, int row, int *x, int *y) {
+	*x = wc1[pattern][row][0];
+	*y = wc1[pattern][row][1];
 }
 
 
-
+//checks if a player has won the game
 int checkWin(char arr[][3], int player) {
+	//initialize variables
 	int x;
 	int y;
 	int inarow = 0;
 	char piece;
+
+	//sets piece depending on player
 	if(player == 1) {
 		piece = 'X';
 	}
@@ -30,17 +72,22 @@ int checkWin(char arr[][3], int player) {
 		piece = 'O';
 	}
 	
-	for(int i = 0; i < 3; i++) {
-		coords(i, &x, &y);
-		if(arr[x][y] == piece) {
-			inarow++;
-			cout << inarow;
+	//checks the specified coords of all the win positions
+	//if the specified piece is found in 3 in a row
+	//the function returns the player number
+	//otherwise it returns a 0
+	for(int pattern = 0; pattern < 8; pattern++) {
+		for(int coordset = 0; coordset < 3; coordset++) {
+			coords(pattern, coordset, &x, &y);
+			if(arr[x][y] == piece) {
+				inarow++;
+			}
 		}
+		if(inarow == 3) {
+			return player;
+		}
+		inarow = 0;
 	}
-
-	if(inarow == 3) {
-		return player;
-	}
-
+  
 	return 0;
 }
